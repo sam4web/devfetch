3bfb276f372fb37d899579ad64a9e64b33b3ac8e
Sijal Manandhar <sijal.m06@gmail.com>
2024-05-27 20:15:36 +0545

    tailwindcss & alias setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    http: 'http://localhost',
+    port: 5000,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
